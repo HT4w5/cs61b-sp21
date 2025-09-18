5ee1a9dad2366931bd35fb9dcd58a60972df516f
HT4w5 <ht4w5x@gmail.com>
2025-09-19 00:12:43 +0800

    Implement tilting in all directions (finish project 0)

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -124,43 +124,40 @@ public class Model extends Observable {
         boolean changed;
         changed = false;
 
-        // Implement north first
-        if (side == Side.NORTH) {
-            for (int i = 0; i < size(); ++i) {
-                int sizeAvail = size(); // Limit the available slots to prevent merging newly merged tiles
-                for (int j = size() - 1; j >= 0; --j) {
-                    Tile t = tile(i, j);
-                    // Skip if no tile present
-                    if (t == null) {
+        for (int i = 0; i < size(); ++i) {
+            int sizeAvail = size(); // Limit the available slots to prevent merging newly merged tiles
+            for (int j = size() - 1; j >= 0; --j) {
+                Tile t = tile(side.col(i, j, size()), side.row(i, j, size()));
+                // Skip if no tile present
+                if (t == null) {
+                    continue;
+                }
+
+                boolean move = false;
+                int dstPos = j + 1;
+
+                for (; dstPos < sizeAvail; ++dstPos) {
+                    Tile ct = tile(side.col(i, dstPos, size()), side.row(i, dstPos, size()));
+                    if (ct == null) {
+                        move = true;
                         continue;
                     }
-
-                    boolean move = false;
-                    int dstPos = j + 1;
-
-                    for (; dstPos < sizeAvail; ++dstPos) {
-                        Tile ct = tile(i, dstPos);
-                        if (ct == null) {
-                            move = true;
-                            continue;
-                        }
-                        if (ct.value() == t.value()) {
-                            // Merge
-                            board.move(i, dstPos, t);
-                            changed = true;
-                            score += ct.value() + ct.value();
-                            move = false;
-                            sizeAvail = dstPos;
-                            break;
-                        } else {
-                            break;
-                        }
-                    }
-
-                    if (move) {
-                        board.move(i, dstPos - 1, t);
+                    if (ct.value() == t.value()) {
+                        // Merge
+                        board.move(side.col(i, dstPos, size()), side.row(i, dstPos, size()), t);
                         changed = true;
+                        score += ct.value() + ct.value();
+                        move = false;
+                        sizeAvail = dstPos;
+                        break;
+                    } else {
+                        break;
                     }
+                }
+
+                if (move) {
+                    board.move(side.col(i, dstPos - 1, size()), side.row(i, dstPos - 1, size()), t);
+                    changed = true;
                 }
             }
         }
