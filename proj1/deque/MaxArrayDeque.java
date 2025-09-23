0f66bd0abf6af4beb5fd1f207a1b905a5e672449
HT4w5 <ht4w5x@gmail.com>
2025-09-24 00:27:31 +0800

    Implement MaxArrayDeque

---
+++ proj1/deque/MaxArrayDeque.java
@@ -0,0 +1,44 @@
+package deque;
+
+import java.util.Comparator;
+
+public class MaxArrayDeque<T> extends ArrayDeque<T> {
+    private final Comparator<T> comp_;
+
+    public MaxArrayDeque(Comparator<T> c) {
+        super();
+        comp_ = c;
+    }
+
+    public T max() {
+        if(size()==0) {
+            return null;
+        }
+        var it = iterator();
+        var maxElem = it.next();
+        while(it.hasNext()) {
+            var nextElem = it.next();
+            if(comp_.compare(nextElem, maxElem) > 0) {
+                maxElem = nextElem;
+            }
+        }
+
+        return maxElem;
+    }
+
+    public T max(Comparator<T> c) {
+        if(size()==0) {
+            return null;
+        }
+        var it = iterator();
+        var maxElem = it.next();
+        while(it.hasNext()) {
+            var nextElem = it.next();
+            if(c.compare(nextElem, maxElem) > 0) {
+                maxElem = nextElem;
+            }
+        }
+
+        return maxElem;
+    }
+}
