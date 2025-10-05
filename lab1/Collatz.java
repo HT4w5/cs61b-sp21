4405addc01690d2aaa7359c559921c691feda2c2
HT4w5 <ht4w5x@gmail.com>
2025-10-05 20:55:08 +0800

    lab4a: implement lab1 collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,11 +1,29 @@
-/** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+/**
+ * Class that prints the Collatz sequence starting from a given number.
+ * 
+ * @author YOUR NAME HERE
  */
 public class Collatz {
+
+    public static int nextNumber(int n) {
+        if (n == 1) {
+            throw new RuntimeException("1 is the last number");
+        }
+
+        if (n % 2 == 0) {
+            return n / 2;
+        } else {
+            return 3 * n + 1;
+        }
+    }
+
     public static void main(String[] args) {
         int n = 5;
         System.out.print(n + " ");
-
+        while (n != 1) {
+            n = nextNumber(n);
+            System.out.print(n + " ");
+        }
+        System.out.println();
     }
 }
-
