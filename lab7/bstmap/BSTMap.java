d0db7e43cb2784ed428f04b17d20f0af27af18f6
HT4w5 <ht4w5x@gmail.com>
2025-10-07 16:20:21 +0800

    lab7: BSTMap: design private members

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -2,6 +2,7 @@ package bstmap;
 
 import java.util.Iterator;
 import java.util.Set;
+import java.util.Stack;
 
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     // Public interface
@@ -30,7 +31,8 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     }
 
     /* Associates the specified value with the specified key in this map. */
-    public void put(K key, V value) {}
+    public void put(K key, V value) {
+    }
 
     /* Returns a Set view of the keys contained in this map. */
     public Set<K> keySet() {
@@ -50,5 +52,44 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
     public Iterator<K> iterator() {
         return null;
+    }
+
+    // Constructor
+    public BSTMap() {
+        // Create sentinel node
+        sentinel_ = makeSentinelNode();
+        root_ = sentinel_;
+    }
+
+    // Private methods
+    private static class BSTNode<K, V> {
+        BSTNode<K, V> left_;
+        BSTNode<K, V> right_;
+        K key_;
+        V value_;
+    }
+
+    private int size_ = 0;
+    private BSTNode<K, V> root_;
+    // Sentinel node attaches to leaf nodes
+    private BSTNode<K, V> sentinel_;
+
+    // Private methods
+    private BSTNode<K, V> makeNode(K k, V v) {
+        BSTNode<K, V> node = new BSTNode<>();
+        node.left_ = sentinel_;
+        node.right_ = sentinel_;
+        node.key_ = k;
+        node.value_ = v;
+        return node;
+    }
+
+    private BSTNode<K, V> makeSentinelNode() {
+        BSTNode<K, V> node = new BSTNode<>();
+        node.left_ = null;
+        node.right_ = null;
+        node.key_ = null;
+        node.value_ = null;
+        return node;
     }
 }
