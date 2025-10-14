cb54dcd96815951278a011ba0d6e15a63205af92
HT4w5 <ht4w5x@gmail.com>
2025-10-14 14:10:21 +0800

    Main: prompt on empty params

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -1,7 +1,7 @@
 package gitlet;
 
 /** Driver class for Gitlet, a subset of the Git version-control system.
- *  @author TODO
+ *  @author John Doe
  */
 public class Main {
 
@@ -9,7 +9,10 @@ public class Main {
      *  <COMMAND> <OPERAND1> <OPERAND2> ... 
      */
     public static void main(String[] args) {
-        // TODO: what if args is empty?
+        if(args.length == 0) {
+            System.out.println("Please enter a command.");
+            System.exit(0);
+        }
         String firstArg = args[0];
         switch(firstArg) {
             case "init":
